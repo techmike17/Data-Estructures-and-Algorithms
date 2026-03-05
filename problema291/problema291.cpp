08085590beea50b8775a218e3e583ab8f1b1d5d4
mperezIROB <165656721+mperezIROB@users.noreply.github.com>
2026-03-05 11:57:00 -0600

    accepted

--- problema291/problema291.cpp
+++ problema291/problema291.cpp
@@ -10,41 +10,49 @@ using namespace std;
 // double linked list with struct or whatever but using classes
 int main()
 {
-    string line;
-    getline(cin, line);
-    int nLine = 1;
-    map<string, vector<int>> words;
-    getline(cin, line);
-
-    while (line != "0")
+    int nLines;
+    cin >> nLines;
+    while (nLines != 0)
     {
-        stringstream ss(line);
-        string word;
+        char aux;
+        cin.get(aux);
+        string line;
+        map<string, vector<int>> words;
 
-        while (ss >> word) // separa por espacios automáticamente
+        for (int i = 1; i <= nLines; i++)
         {
+            getline(cin, line);
+            stringstream ss(line);
+            string word;
 
-            if (word.size() > 2)
+            while (ss >> word) // separa por espacios automáticamente
             {
-                transform(word.begin(), word.end(), word.begin(), ::tolower);
 
-                if (!words.count(word))
-                    words[word] = {nLine};
-                else if (words[word].back() < nLine)
-                    words[word].push_back(nLine);
+                if (word.size() > 2)
+                {
+                    transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+                    if (!words.count(word))
+                        words[word] = {i};
+                    else if (words[word].back() < i)
+                        words[word].push_back(i);
+                }
             }
         }
 
-        nLine++;
-        getline(cin, line);
+        for (auto const &x : words)
+        {
+            cout << x.first << ' ';
+            for (int i = 0; i < x.second.size(); i++)
+            {
+                cout << x.second[i];
+                if (i < x.second.size() - 1)
+                    cout << ' ';
+            }
+            cout << '\n';
+        }
+        cout << "----\n";
+        cin >> nLines; // ✅ convierte string a int
     }
-    for (auto const &x : words)
-    {
-        cout << x.first;
-        for (int i : x.second)
-            cout << " " << i;
-        cout << "\n";
-    }
-    cout << "---\n";
     return 0;
 }
