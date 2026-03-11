b08335292a664e6dc0d61b69493510a69ffeb58b
mperezIROB <165656721+mperezIROB@users.noreply.github.com>
2026-03-11 12:31:39 -0500

    need a break

--- problema612/problema612.cpp
+++ problema612/problema612.cpp
@@ -11,60 +11,18 @@ double euclideanDistance(int radio, int x1, int y1, int x2, int y2)
     return distance;
 }
 
-int numPuntosCirculos(int radio, int x2, int y2)
+int numPuntosCirculos(int radio, int x1, int y1, int x2, int y2)
 {
     int res = 0;
     double distance = 0;
     while (radio > 0)
     {
-        distance = euclideanDistance(radio, 0, 0, x2, y2);
+        distance = euclideanDistance(radio, x1, y1, x2, y2);
         if (distance <= radio)
             res++;
         if (radio > 1)
         {
-            int nextX = abs(x2 - radio);
-            int nextY = abs(y2 - radio);
-
-            if (nextY < nextX)
-            {
-                // next point is up or down
-                if (y2 < 0) // go to down
-                {
-                    y2 = y2 - radio;
-                }
-                else if (y2 > 0) // go to up
-                {
-                    y2 = y2 - radio;
-                }
-            }
-            else if (nextY > nextX)
-            {
-                // next point is left or right
-                if (x2 < 0) // go to the left
-                {
-                    x2 = x2 + radio;
-                }
-                else if (x2 > 0) // go to the right
-                {
-                    x2 = x2 - radio;
-                }
-            }
-            else
-            {
-                // the point is on a diagonal doesnt matter axis Y or X
-                if (x2 < 0) // go to the left
-                {
-                    x2 = x2 + radio;
-                }
-                else if (x2 > 0) // go to the right
-                {
-                    x2 = x2 - radio;
-                }
-                else // center of a circunference
-                    break;
-            }
         }
-        radio = radio / 2;
     }
     return res;
 }
