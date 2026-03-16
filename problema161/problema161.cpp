5a1215637d9cbeb780fa3f4e74139f426fa77c35
techmike17 <migperezdlrubia@gmail.com>
2026-03-16 10:58:42 -0500

    problem 161 acc

--- problema161/problema161.cpp
+++ problema161/problema161.cpp
@@ -7,17 +7,32 @@
 #include <sstream>
 
 using namespace std;
-// double linked list with struct or whatever but using classes
+
 int main()
 {
     int nLines;
     cin >> nLines;
     while (nLines != 0)
     {
-        vector<int> nums;
+
+        vector<int> dll;
+
+        // Insert elements at the end
+        int num;
         for (int i = 0; i < nLines; i++)
         {
+            cin >> num;
+            dll.push_back(num);
         }
+
+        sort(dll.begin(), dll.end());
+
+        int med = nLines / 2;
+        if (nLines % 2)
+            cout << dll[med] * 2 << "\n";
+        else
+            cout << dll[med - 1] + dll[med] << "\n";
+
         cin >> nLines;
     }
     return 0;
