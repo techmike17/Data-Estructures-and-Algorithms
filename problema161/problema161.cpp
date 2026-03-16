3491d9919a31cd1304e3f1df063c8d11877edf17
mperezIROB <165656721+mperezIROB@users.noreply.github.com>
2026-03-16 05:37:33 -0500

    starting problem 161

---
+++ problema161/problema161.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+// double linked list with struct or whatever but using classes
+int main()
+{
+    int nLines;
+    cin >> nLines;
+    while (nLines != 0)
+    {
+        vector<int> nums;
+        for (int i = 0; i < nLines; i++)
+        {
+        }
+        cin >> nLines;
+    }
+    return 0;
+}
