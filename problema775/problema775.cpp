d72c420f246837671d38d4bc9905290e8c33d06c
techmike17 <migperezdlrubia@gmail.com>
2026-04-08 04:42:34 -0500

    still WA deleted first if due to errors

--- problema775/problema775.cpp
+++ problema775/problema775.cpp
@@ -9,33 +9,26 @@ int main()
 {
     double num;
     double acum = 0;
-    double last;
+    double last = 0;
 
     cin >> num;
     while (num)
     {
         do
         {
-            if (acum < num)
+
+            if ((acum - last) < num)
             {
-                acum += num;
+                acum = acum + (num - (acum - last));
             }
-            else
-            {
-                if (last <= num)
-                {
-                    if ((acum - last) < num)
-                    {
-                        acum = acum + (num - (acum - last));
-                    }
-                }
-            }
+
             last = num;
             cin >> num;
         } while (num != 0);
         cout << acum << "\n";
         cin >> num;
         acum = 0;
+        last = 0;
     }
 
     return 0;
